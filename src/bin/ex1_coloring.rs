2dd1fd93986dedc069bc39a8db6da955c3512009
Tom Kiesow <tom@kiesow.email>
2024-04-28 22:58:49 +0200

    switch default encoding for coloring

--- src/bin/ex1_coloring.rs
+++ src/bin/ex1_coloring.rs
@@ -1,13 +1,13 @@
 use std::env;
 
-use practical_sat::ex1::coloring::{FindKResult, hybrid};
+use practical_sat::ex1::coloring::{FindKResult, one_hot_incremental};
 use practical_sat::ex1::coloring::graph::Graph;
 use practical_sat::util::Timer;
 
 fn main() {
     let graph_path = env::args().last().unwrap();
     let graph = Graph::parse_dimacs(graph_path);
-    match hybrid::find_k(graph, Timer::new_infinite()) {
+    match one_hot_incremental::find_k(graph, Timer::new_infinite()) {
         FindKResult::Found(k) => println!("{k}"),
         x => println!("{x:?}"),
     }
